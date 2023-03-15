53eaf321da583515d26f737ef48db412503956ff
Fil Sandalo Jr <filadelfo.sandalo.jr@prudential.com.sg>
2023-03-15 10:44:43 +0800

    test

--- src/components/Trigger.vue
+++ src/components/Trigger.vue
@@ -128,7 +128,19 @@ export default {
     }
 
     onMounted(() => {
-
+      window.addEventListener('message', event => {
+        // IMPORTANT: check the origin of the data!
+        if (event.origin === 'http://pamb-uat.litmos.com.au/') {
+          // The data was sent from your site.
+          // Data sent with postMessage is stored in event.data:
+          console.log(event.data);
+        } else {
+          // The data was NOT sent from your site!
+          // Be careful! Do not use it. This else branch is
+          // here just for clarity, you usually shouldn't need it.
+          return;
+        }
+      });
     });
 
 
