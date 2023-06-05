fbba754cfacd083a26689b270796030875c7ad74
Fil Sandalo Jr <filadelfo.sandalo.jr@prudential.com.sg>
2023-06-05 16:47:36 +0800

    add pagination

--- src/components/nextgen/Comments.vue
+++ src/components/nextgen/Comments.vue
@@ -7,43 +7,56 @@ import Comment from "./Comment.vue";
     <div class="text-center mb-4">
       <p style="font-weight: 600; font-size: 26px; color: #DA291C">CHECK OUT WHAT FELLOW PEERS HAVE TO SAY!</p>
       <hr/>
-      <p style="font-weight: 600;font-size: 16px;">TOTAL COMMENTS: {{data.value.length}}</p>
+      <p style="font-weight: 600;font-size: 16px;">TOTAL COMMENTS: {{data.value.total}}</p>
     </div>
-    <div  v-for="(item, index) in data.value">
+    <div  v-for="(item, index) in data.value.data">
       <Comment :data="item" />
     </div>
+    <Bootstrap5Pagination
+        size="large"
+        align="center"
+        :data="data.value"
+        @pagination-change-page="getComments"
+    />
   </div>
+
 </template>
 
 <script setup>
 import { ref, reactive, onMounted } from 'vue'
 import axios from "axios";
 import { useRoute } from 'vue-router'
-
+import { Bootstrap5Pagination } from 'laravel-vue-pagination';
+const router = useRoute();
 const data = reactive({value: []});
+const moduleId = router.query.moduleId;
 
 onMounted(() => {
-  const router = useRoute();
+  getComments(1)
+})
 
+const getComments = (page) => {
   axios({
     method: 'get',
     url: 'https://shark-app-pjbx4.ondigitalocean.app/api/comments',
     // url: 'http://localhost:8001/api/comments',
     params: {
-      // username: router.query.username,
+      page: page,
       // password: password.value,
       // title: router.query.title,
-      moduleId: router.query.moduleId,
+      moduleId: moduleId,
       // moduleId: 2036359,
       lbu: 'PAMB',
     }
   }).then((resp) => {
     data.value = resp.data;
-    console.log(data);
+    console.log(data.value);
   })
-})
+}
 </script>
 
-<style scoped>
-
+<style>
+.page-item.active .page-link {
+  background-color: #ED1B2E !important;
+}
 </style>
